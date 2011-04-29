b6fb1e7de1158efd1ad130a6d6fc8c68e43dbd12
paulox <paulox@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-29 12:40:56 +0000

    Updated to 1.3 version.
    git-svn-id: https://plugins.svn.wordpress.org/send-e-mail/trunk@378763 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -2,15 +2,17 @@
 Contributors: paulox
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=10617157
 Tags: akismet, contact, contact form, email, i18n, international, l10n, language, localization, .mo file, multilingual, plugin, .po file, signature, wordpress.com
-Stable tag: 1.2
+Stable tag: 1.3
 Requires at least: 2.5
-Tested up to: 2.9
+Tested up to: 3.1
 
-Add a contact form to any post or page by inserting `[contact-form]` in the post. Emails will be sent to the post's author. As seen on WordPress.com.
+Add a contact form to any post, page or text widget. Messages will be sent to any email address you choose. As seen on wordpress.com with added i18n.
 
 == Description ==
 
-Add a contact form to any post or page by inserting `[contact-form]` in the post. Emails will be sent to the post's author.
+Add a contact form to any post or page by inserting `[contact-form]` in the post. Messages will be sent to the post's author or any email address you choose.
+
+Or add a contact form to a text widget.  Messages will be sent to the email address set in your Settings -> General admin panel or any email address you choose.
 
 Your email address is never shown, and the sender never learns it (unless you reply to the email).
 
@@ -19,6 +21,28 @@ As seen on WordPress.com.
 This plugin was imported from [Grunion Contact Form](http://wordpress.org/extend/plugins/grunion-contact-form/ "Grunion Contact Form - wordpress.org") but it adds full internationalization using localized string from Wordpress core.
 
 An e-mail [signature block](http://en.wikipedia.org/wiki/Signature_block "Signature block - Wikipedia") was appended to the end of the e-mail message containing the sender's name, email address, website and IP and delimited from the body of the message by a single line consisting of exactly two hyphens, followed by a space, followed by the end of line ("-- \n"). This [signature cut line](http://tools.ietf.org/html/rfc3676#section-4.3 "Usenet Signature Convention - RFC3676 - IETF") allows software to automatically mark or remove the sig block.
+
+= Configuration =
+
+The `[contact-form]` shortcode has the following parameters:
+
+* `to`: A comma separated list of email addresses to which the messages will be sent.
+  If you leave this blank: contact forms in posts and pages will send messages to the post or page's author; and
+  contact forms in text widgets will send messages to the email address set in Settings -> General.
+
+  Example: `[contact-form to="you@me.com"]`
+
+  Example: `[contact-form to="you@me.com,me@you.com,us@them.com"]`
+
+* `subject`: The e-mail subject of the message defaults to `[{Blog Title}] {Sidebar}` for text widgets
+  and `[{Blog Title}] {Post Title}` for posts and pages. Set your own default with the subject option.
+
+  Example: `[contact-form subject="My Contact Form"]`
+
+* `show_subject`: You can allow the user to edit the subject by showing a new field on the form. The
+  field will be populated with the default subject or the subject you have set with the previous option.
+
+  Example: `[contact-form subject="My Contact Form" show_subject="yes"]`
 
 == Installation ==
 
@@ -34,7 +58,7 @@ This plugin was full internationalized because use localized string from Wordpre
 
 = What about spam? Will I get a lot from the contact form? =
 
-If you have [Akismet](http://akismet.com "Akismet Site") installed on your blog, you may get some spam, but you shouldn't get a lot.
+If you have [Akismet](http://akismet.com "Akismet Site") installed on your blog, you shouldn't get much spam.
 All the messages people send to you through the contact form will be filtered through Akismet.
 
 = Anyone can put whatever they want in the name and email boxes. How can I know who's really sending the message? =
@@ -46,7 +70,9 @@ Anonymity is both a curse and a blessing :)
 
 = My blog has multiple authors. Who gets the email? =
 
-The email is sent to the author of the post with the contact form in it. So each author on your blog can have his or her own contact form.
+By default, the email is sent to the author of the post with the contact form in it. So each author on your blog can have his or her own contact form.
+
+In the contact form shortcode, you can specify what email address(es) messages should be sent to with the `to` parameter.
 
 = Great! But how will my visitors know who they're sending a message to? =
 
@@ -55,10 +81,21 @@ Just make the title of your post "Contact Mary" or put "Hey, drop John a line wi
 == Screenshots ==
 
 1. Send E-mail - English
-2. Send E-mail - Espanol
+2. Send E-mail - Español
 3. Send E-mail - Italiano
 
 == Changelog ==
+
+= 1.3 =
+* Tested with WordPress 3.1.
+* More internationalized text.
+* Fixed some typo.
+* This version is aligned with the 1.2 version of Grunion Contact Form.
+* Fix a PHP Warning in some CGI environments.
+* Move to shortcode API.
+* Add `to`, `subject` and `show-subject` options.
+* Allow use in text widgets.
+* Move spam check to a filter.
 
 = 1.2 =
 * Tested with WordPress 2.9
@@ -70,6 +107,9 @@ Just make the title of your post "Contact Mary" or put "Hey, drop John a line wi
 
 == Upgrade Notice ==
 
+= 1.3 =
+Now with more internationalized text, options and a fixes.
+
 = 1.2 =
 This version was tested with WordPress 2.9
 
@@ -80,7 +120,7 @@ This version adds full internationalization to plugin using localized string fro
 
 = Send E-mail =
 
-Copyright (C) 2009 Paolo Melchiorre
+Copyright (C) 2011 Paolo Melchiorre
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
--- send-e-mail.php
+++ send-e-mail.php
@@ -2,154 +2,204 @@
 
 /*
 Plugin Name: Send E-mail
-Description: Add a contact form to any post just by inserting <code>[contact-form]</code> in the post. Emails will be sent to the post's author. As seen on WordPress.com.
+Description: Add a contact form to any post, page or text widget. Messages will be sent to any email address you choose. As seen on wordpress.com with added i18n.
 Plugin URI: http://www.paulox.net/software-libero/send-e-mail
 Author: Paolo Melchiorre
 Author URI: http://www.paulox.net
-Version: 1.2
+Version: 1.3
 */
 
-// TODO: Move to shortcode API
+function contact_form_shortcode( $atts ) {
+	global $post;
 
-function contact_form_shortcode( $content ) {
-	if ( false !== strpos($content, '[contact-form]') )
-		return preg_replace_callback('|(<p>)?\[contact-form\](</p>)?|i', 'contact_form_html', $content);
-	return $content;
-}
+	$default_to = get_option( 'admin_email' );
+	$default_subject = "[" . get_option( 'blogname' ) . "]";
 
-function contact_form_html() {
+	if ( $atts['widget'] ) {
+		$default_subject .=  " Sidebar";
+	} elseif ( $post->ID ) {
+		$default_subject .= " ". wp_kses( $post->post_title, array() );
+		$post_author = get_userdata( $post->post_author );
+		$default_to = $post_author->user_email;
+	}
+
+	extract( shortcode_atts( array(
+		'to' => $default_to,
+		'subject' => $default_subject,
+		'show_subject' => 'no',
+		'widget' => 0 //This is not exposed to the user. Works with contact_form_widget_atts
+	), $atts ) );
+
 	if ( ( function_exists( 'faux_faux' ) && faux_faux() ) || is_feed() )
 		return '[contact-form]';
 
-	global $wp_query, $contact_form_errors, $contact_form_values, $user_identity;
-	if ( is_singular() )
+	global $wp_query, $contact_form_errors, $contact_form_values, $user_identity, $contact_form_last_id;
+
+	if ( $widget )
+		$id = 'widget-' . $widget;
+	elseif ( is_singular() )
 		$id = $wp_query->get_queried_object_id();
 	else
 		$id = $GLOBALS['post']->ID;
 	if ( !$id ) // something terrible has happened
 		return '[contact-form]';
 
-	$permalink = get_permalink( $id );
+	if ( $id == $contact_form_last_id )
+		return;
+	else
+		$contact_form_last_id = $id;
 
 	ob_start();
 		wp_nonce_field( 'contact-form_' . $id );
 		$nonce = ob_get_contents();
 	ob_end_clean();
 
-	if ( is_array($contact_form_values) )
-		extract($contact_form_values);
+	$message_sent = contact_form_send_message( $to, $subject, $widget );
 
-	foreach ( array('comment_author', 'comment_author_email', 'comment_author_url') as $k )
-		if ( !isset($$k) )
+	if ( is_array( $contact_form_values ) )
+		extract( $contact_form_values );
+
+	foreach ( array( 'comment_author', 'comment_author_email', 'comment_author_url' ) as $k )
+		if ( !isset( $$k ) )
 			$$k = '';
 		else
 			$$k = attribute_escape( $$k );
 	$comment_author_url = $comment_author_url ? $comment_author_url : 'http://';
 
-	if ( !isset($comment_content) )
+	if ( !isset( $comment_content ) )
 		$comment_content = '';
 	else
 		$comment_content = wp_specialchars( $comment_content );
 
+	$emails = str_replace( ' ', '', $to );
+	$emails = explode( ',', $emails );
+	foreach ( (array) $emails as $email ) {
+		if ( is_email( $email ) && ( !function_exists( 'is_email_address_unsafe' ) || !is_email_address_unsafe( $email ) ) )
+			$valid_emails[] = $email;
+	}
+	$to = ( $valid_emails ) ? $valid_emails : $default_to;
+
 	$r = "<div id='contact-form-$id'>\n";
 
 	$errors = array();
-	if ( is_wp_error($contact_form_errors) && $errors = (array) $contact_form_errors->get_error_codes() ) :
-		$r .= "<div class='form-error'>\n<h3>" . __('Error!') . "</h3>\n<p>\n";
+	if ( is_wp_error( $contact_form_errors ) && $errors = (array) $contact_form_errors->get_error_codes() ) :
+		$r .= "<div class='form-error'>\n<h3>" . __( "Caution:" ) . "</h3>\n<p>\n";
 		foreach ( $contact_form_errors->get_error_messages() as $message )
 			$r .= "\t$message<br />\n";
 		$r .= "</p>\n</div>\n\n";
-	elseif ( !empty($_POST) ) :
-		$r .= "<address>" . __('Submitted') . "</address>\n\n";
+	elseif ( $message_sent ) :
+		$r .= "<h3>" . __( 'Success!' ) . "</h3>\n\n";
 		$r .= wpautop( $comment_content ) . "</div>";
+		
+		// Reset for multiple contact forms. Hacky
+		$contact_form_values['comment_content'] = '';
+
 		return $r;
 	endif;
 
-	$r .= "<form action='$permalink#contact-form-$id' method='post' class='contact-form commentsblock'>\n";
+	$r .= "<form action='#contact-form-$id' method='post' class='contact-form commentsblock'>\n";
 	if ( is_user_logged_in() ) :
-		$r .= "\t<p>" . sprintf(__( "Logged in as %s." ), '<a href="'.get_option('siteurl').'/wp-admin/profile.php">'.$user_identity.'</a>') . " <a href='" . wp_logout_url(get_permalink()) . "' title='" . __('Log out of this account') . "'>" . __('Log out &raquo;') . "</a></p>\n";
+		$r .= "\t<p>" . sprintf( __('Logged in as <a href="%1$s">%2$s</a>.'), get_option('siteurl') . '/wp-admin/profile.php', $user_identity ) . " <a href='" . wp_logout_url(get_permalink()) . "' title='" . __('Log out of this account') . "'>" . __('Log out &raquo;') . "</a></p>\n";
 	else :
 		$r .= "\n<p>\n";
 		$r .= "\t\t<input type='text' name='comment_author' id='name-$id' value='$comment_author' class='textbox' size='35' tabindex='1' />\n";
-		$r .= "\t\t<label for='name-$id' class='name" . ( in_array('comment_author', $errors) ? ' form-error' : '' ) . "'>" . __('Name') ." <small><em>" . __('(required)') . "</em></small></label>\n";
+		$r .= "\t\t<label for='name-$id' class='name" . ( in_array( 'comment_author', $errors ) ? ' form-error' : '' ) . "'>" . __('Name') ." <small><em>" . __('(required)') . "</em></small></label>\n";
 		$r .= "\t</p>\n";
 		$r .= "\n<p>\n";
 		$r .= "\t\t<input type='text' name='comment_author_email' id='email-$id' value='$comment_author_email' class='textbox' size='35' tabindex='2' />\n";
-		$r .= "\t\t<label for='name-$id' class='email" . ( in_array('comment_author_email', $errors) ? ' form-error' : '' ) . "'>" . __('E-mail') ." <small><em>" . __('(required)') . "</em></small></label>\n";
+		$r .= "\t\t<label for='email-$id' class='email" . ( in_array( 'comment_author_email', $errors ) ? ' form-error' : '' ) . "'>" . __('E-mail') ." <small><em>" . __('(required)') . "</em></small></label>\n";
 		$r .= "\t</p>\n";
 		$r .= "\n<p>\n";
 		$r .= "\t\t<input type='text' name='comment_author_url' id='url-$id' value='$comment_author_url' class='textbox' size='35' tabindex='3' />\n";
-		$r .= "\t\t<label for='name-$id' class='url'>" . __('Website') . "</label>\n";
+		$r .= "\t\t<label for='url-$id' class='url'>" . __( 'Website' ) . "</label>\n";
 		$r .= "\t</p>\n";
 	endif;
+	if ( 'yes' == strtolower( $show_subject ) ) {
+		$r .= "\n<p>\n";
+		$r .= "\t\t<input type='text' name='contact_form_subject' id='subject-$id' value='" . esc_attr( $subject ) . "' class='textbox'/>\n";
+		$r .= "\t\t<label for='subject-$id' class='subject'>" . __( 'Subject' ) . "</label>\n";
+		$r .= "\t</p>\n";
+	}
 	$r .= "\n<p>\n";
 	$r .= "\t\t<textarea name='comment_content' id='contact-form-comment-$id' cols='60' rows='10' tabindex='4'>$comment_content</textarea>\n";
 	$r .= "\t</p>\n";
 	$r .= "\t<p class='contact-submit'>\n";
-	$r .= "\t\t<input style=\"text-transform:capitalize\" type='submit' tabindex='5' value='" . __( "send e-mail" ) . "' class='pushbutton-wide'/>\n";
+	$r .= "\t\t<input style='text-transform:capitalize' type='submit' tabindex='5' value='" . __( "send e-mail" ) . "' class='pushbutton-wide'/>\n";
 	$r .= "\t\t$nonce\n";
 	$r .= "\t\t<input type='hidden' name='contact-form-id' value='$id' />\n";
 	$r .= "\t</p>\n";
 	$r .= "</form>\n</div>";
+
 	return $r;
 }
+add_shortcode( 'contact-form', 'contact_form_shortcode' );
 
-function contact_form_pre_head() {
-	global $post, $current_user, $user_identity;
-	if ( !is_singular() || false === strpos($post->post_content, '[contact-form]') )
+function contact_form_send_message( $to, $subject, $widget ) {
+	global $post;
+
+ 	if ( !isset( $_POST['contact-form-id'] ) )
+		return;
+		
+	if ( ( $widget && 'widget-' . $widget != $_POST['contact-form-id'] ) || ( !$widget && $post->ID != $_POST['contact-form-id'] ) )
 		return;
 
- 	if ( !isset($_POST['contact-form-id']) )
-		return;
+	if ( $widget )
+		check_admin_referer( 'contact-form_widget-' . $widget );
+	else
+		check_admin_referer( 'contact-form_' . $post->ID );
 
-	check_admin_referer( 'contact-form_' . $post->ID );
-
-	global $contact_form_values, $contact_form_errors;
+	global $contact_form_values, $contact_form_errors, $current_user, $user_identity;
 
 	$contact_form_values = array();
 	$contact_form_errors = new WP_Error();
 
 	list($comment_author, $comment_author_email, $comment_author_url) = is_user_logged_in() ?
-		add_magic_quotes(array($user_identity, $current_user->data->user_email, $current_user->data->user_url)) :
-		array($_POST['comment_author'], $_POST['comment_author_email'], $_POST['comment_author_url']);
+		add_magic_quotes( array( $user_identity, $current_user->data->user_email, $current_user->data->user_url ) ) :
+		array( $_POST['comment_author'], $_POST['comment_author_email'], $_POST['comment_author_url'] );
 
-	if ( !$comment_author = stripslashes(apply_filters( 'pre_comment_author_name', $comment_author )) )
-		$contact_form_errors->add( 'comment_author', __('You must enter your name.') );
+	if ( !$comment_author = stripslashes( apply_filters( 'pre_comment_author_name', $comment_author ) ) )
+		$contact_form_errors->add( 'comment_author', __('Error: please fill the required fields (name, email).') );
 
-	$comment_author_email = stripslashes(apply_filters( 'pre_comment_author_email', $comment_author_email ));
-	if ( !is_email($comment_author_email) )
-		$contact_form_errors->add( 'comment_author_email', __('You must enter a valid email address.') );
+	$comment_author_email = stripslashes( apply_filters( 'pre_comment_author_email', $comment_author_email ) );
+	if ( !is_email( $comment_author_email ) )
+		$contact_form_errors->add( 'comment_author_email', __( 'Error: please enter a valid email address.' ) );
 
-	$comment_author_url = stripslashes(apply_filters( 'pre_comment_author_url', $comment_author_url ));
+	$comment_author_url = stripslashes( apply_filters( 'pre_comment_author_url', $comment_author_url ) );
 	if ( 'http://' == $comment_author_url )
 		$comment_author_url = '';
 
-	$comment_content = stripslashes($_POST['comment_content']);
-	$comment_content = trim(wp_kses( $comment_content, array() ));
+	$comment_content = stripslashes( $_POST['comment_content'] );
+	$comment_content = trim( wp_kses( $comment_content, array() ) );
 	if ( !$comment_content )
-		$contact_form_errors->add( 'comment_content', __('You did not enter a comment!') );
+		$contact_form_errors->add( 'comment_content', __( 'Error: please type a comment.' ) );
 
-	$vars = array('comment_author', 'comment_author_email', 'comment_author_url');
+	$contact_form_subject = stripslashes( $_POST['contact_form_subject'] );
+	$contact_form_subject = trim( wp_kses( $contact_form_subject, array() ) );
+	if ( !$contact_form_subject )
+		$contact_form_subject = $subject;
+
+	$vars = array( 'comment_author', 'comment_author_email', 'comment_author_url', 'contact_form_subject' );
 	foreach ( $vars as $var )
-		$$var = str_replace(array("\n", "\r"), '', $$var); // I don't know if it's possible to inject this
+		$$var = str_replace( array("\n", "\r" ), '', $$var ); // I don't know if it's possible to inject this
 	$vars[] = 'comment_content';
 
-	$contact_form_values = compact($vars);
+	$contact_form_values = compact( $vars );
 
 	if ( $contact_form_errors->get_error_codes() )
 		return;
 
-	if ( contact_form_is_spam( $contact_form_values ) )
-		return;
+	$spam = '';
+	$is_spam = contact_form_is_spam( $contact_form_values );
+	if ( is_wp_error( $is_spam ) )
+		return; // abort
+	else if ( $is_spam )
+		$spam = '***SPAM*** ';
 
-	$post_author = get_userdata( $post->post_author );
-
-	$headers =	"MIME-Version: 1.0\n" .
-			"From: $comment_author <$comment_author_email>\n" .
+	$headers =	"From: $comment_author <$comment_author_email>\n" .
 			"Reply-To: $comment_author_email\n" .
 			"Content-Type: text/plain; charset=\"" . get_option('blog_charset') . "\"\n";
 
-	$subject = "[" . get_option( 'blogname' ) . "] " . wp_kses( $post->post_title, array() );
+	$subject = apply_filters( 'contact_form_subject', $spam . $contact_form_subject );
 	$time_string = __('Y-m-d G:i:s');
 	$time = date_i18n( __($time_string), current_time( 'timestamp' ) );
 	$ip = preg_replace( '/[^0-9., ]/', '', $_SERVER['REMOTE_ADDR'] );
@@ -175,13 +225,23 @@ function contact_form_pre_head() {
 	$message .= ": $ip
 ";
 
-	wp_mail( $post_author->user_email, $subject, $message, $headers );
+	$message = apply_filters( 'contact_form_message', $message );
+
+	$to = apply_filters( 'contact_form_to', $to );
+
+	return wp_mail( $to, $subject, $message, $headers );
 }
 
+/*
+ * @return	true: it's spam, mark it as such
+ * 		false: it's not spam, let it ride
+ * 		WP_Error: it's spam, abort
+ */
 function contact_form_is_spam( $form ) {
-	if ( !function_exists( 'akismet_http_post' ) )
-		return false;
+	return apply_filters( 'contact_form_is_spam', false, $form );
+}
 
+function contact_form_is_spam_akismet( $return, $form ) {
 	global $akismet_api_host, $akismet_api_port;
 
 	$form['comment_type'] = 'contact_form';
@@ -193,26 +253,42 @@ function contact_form_is_spam( $form ) {
 	$ignore = array( 'HTTP_COOKIE' );
 
 	foreach ( $_SERVER as $k => $value )
-		if ( !in_array($k, $ignore) && is_string( $value ) )
+		if ( !in_array( $k, $ignore ) && is_string( $value ) )
 			$form["$k"] = $value;
 
 	$query_string = '';
-	foreach ( array_keys($form) as $k )
+	foreach ( array_keys( $form ) as $k )
 		$query_string .= $k . '=' . urlencode( $form[$k] ) . '&';
 
-	$response = akismet_http_post($query_string, $akismet_api_host, '/1.1/comment-check', $akismet_api_port);
-	if ( 'true' == trim($response[1]) ) // true is spam
-		return true;
-	return false;
+	$response = akismet_http_post( $query_string, $akismet_api_host, '/1.1/comment-check', $akismet_api_port );
+	if ( 'true' == trim( $response[1] ) ) // 'true' is spam
+		return new WP_Error( 'akismet' ); // abort
+	return $return;
 }
 
-function contact_form_auto_p( $text ) {
-	$text = str_replace( '[contact-form]', "\n[contact-form]\n", $text);
-	return preg_replace( '|[\s]*\[contact-form\][\s]*|', "\n\n[contact-form]\n\n", $text );
+function contact_form_widget_atts( $text ) {
+	static $widget = 0;
+	
+	$widget++;
+
+	return str_replace( '[contact-form', '[contact-form widget="' . $widget . '"', $text );
+}
+add_filter( 'widget_text', 'contact_form_widget_atts', 0 );
+
+function contact_form_widget_shortcode_hack( $text ) {
+	$old = $GLOBALS['shortcode_tags'];
+	remove_all_shortcodes();
+	add_shortcode( 'contact-form', 'contact_form_shortcode' );
+	$text = do_shortcode( $text );
+	$GLOBALS['shortcode_tags'] = $old;
+	return $text;
 }
 
-add_filter( 'content_save_pre', 'contact_form_auto_p' );
+function contact_form_init() {
+	if ( function_exists( 'akismet_http_post' ) )
+		add_filter( 'contact_form_is_spam', 'contact_form_is_spam_akismet', 10, 2 );
+	if ( !has_filter( 'widget_text', 'do_shortcode' ) )
+		add_filter( 'widget_text', 'contact_form_widget_shortcode_hack', 5 );
+}
+add_action( 'init', 'contact_form_init' );
 
-add_action( 'wp', 'contact_form_pre_head' );
-add_filter( 'the_content', 'contact_form_shortcode' );
-?>
